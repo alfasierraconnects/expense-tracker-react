ce6e321d13dd6fec3eebdb3401aa74c685e297b6
alfasierraconnects <alfasierraconnects@gmail.com>
2024-03-13 16:03:38 +0530

    created Component and exported to other file

---
+++ expense-tracker-react/src/Components/Expense.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function () {
+  return (
+    <div>
+      <h3>Expense Items</h3>
+      <p>Food Rs 10</p>
+      <p>Petrol Rs 100</p>
+      <p>Movies Rs 200</p>
+    </div>
+  );
+}
