849fc9ea099ddaf5a14415a428876f6a479733cb
alfasierraconnects <alfasierraconnects@gmail.com>
2024-03-14 13:04:53 +0530

    Styled using TailwindCSS

--- expense-tracker-react/src/Components/Expense.js
+++ expense-tracker-react/src/Components/Expense.js
@@ -1,12 +1,14 @@
 import React from "react";
+import CurrentDate from "./Date";
 
-export default function () {
+export default function Expense() {
   return (
-    <div>
-      <h3>Expense Items</h3>
-      <p>Food Rs 10</p>
-      <p>Petrol Rs 100</p>
-      <p>Movies Rs 200</p>
+    <div className="flex bg-neutral-600 text-white rounded-md p-2 font-medium gap-4 items-center">
+      <CurrentDate />
+      <p className="grow font-bold text-2xl">item</p>
+      <p className="bg-violet-950 py-4 px-6 border-2 rounded-xl font-bold text-2xl">
+        &#8377; 100.00
+      </p>
     </div>
   );
 }
