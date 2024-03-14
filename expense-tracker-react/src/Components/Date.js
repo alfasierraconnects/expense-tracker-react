1d69b6cccec094f49bc246fd9563d9a82c1addcd
alfasierraconnects <alfasierraconnects@gmail.com>
2024-03-14 18:18:58 +0530

    JavaScript Inside JSX

--- expense-tracker-react/src/Components/Date.js
+++ expense-tracker-react/src/Components/Date.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-export default function CurrentDate() {
-  const trailDate = new Date("2017-02-13");
+export default function CurrentDate(props) {
+  const date = props.date;
 
-  const options = { month: "long" };
-  const monthString = trailDate.toLocaleDateString(undefined, options);
+  // Extract year, month, and day separately
+  const yearIST = date.getFullYear();
+  const monthIST = date.toLocaleString("en-IN", { month: "long" });
+  const dayIST = date.getDate();
 
   return (
     <div className="bg-slate-800 p-4 px-6 border-2 rounded-xl flex flex-col items-center">
-      <p className="font-bold">{monthString}</p>
-      <p>{trailDate.getFullYear()}</p>
-      <p className="font-bold text-4xl">{trailDate.getDate()}</p>
+      <p className="font-bold">{yearIST}</p>
+      <p>{monthIST}</p>
+      <p className="font-bold text-4xl">{dayIST}</p>
     </div>
   );
 }
--- expense-tracker-react/src/Components/Expense.js
+++ expense-tracker-react/src/Components/Expense.js
@@ -2,12 +2,15 @@ import React from "react";
 import CurrentDate from "./Date";
 
 export default function Expense() {
+  const item = "Book";
+  const price = 100;
+  const date = new Date(2021, 5, 10);
   return (
     <div className="flex bg-neutral-600 text-white rounded-md p-2 font-medium gap-4 items-center">
-      <CurrentDate />
-      <p className="grow font-bold text-2xl">item</p>
+      <CurrentDate date={date} />
+      <p className="grow font-bold text-2xl">{item}</p>
       <p className="bg-violet-950 py-4 px-6 border-2 rounded-xl font-bold text-2xl">
-        &#8377; 100.00
+        &#8377; {price}
       </p>
     </div>
   );
