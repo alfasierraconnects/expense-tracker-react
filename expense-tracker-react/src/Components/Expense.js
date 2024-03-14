14f9e368c8729b9c4938172d478af99756b52d02
alfasierraconnects <alfasierraconnects@gmail.com>
2024-03-14 19:24:54 +0530

    Reusing components by iterating

--- expense-tracker-react/src/Components/Expense.js
+++ expense-tracker-react/src/Components/Expense.js
@@ -8,6 +8,7 @@ export default function Expense(props) {
   return (
     <div className="flex bg-neutral-600 text-white rounded-md p-2 font-medium gap-4 items-center my-2">
       <CurrentDate date={date} />
+      <p className="w-24 italic text-yellow-200">{props.location}</p>
       <p className="grow font-bold text-2xl">{item}</p>
       <p className="bg-violet-950 py-4 px-6 border-2 rounded-xl font-bold text-2xl w-32">
         &#8377; {price}
