2a8ac3c1e8aca72b49ba406269b7137becdfa72b
alfasierraconnects <alfasierraconnects@gmail.com>
2024-03-16 18:47:51 +0530

    In Form listened to user Input

--- expense-tracker-react/src/Components/NewExpenses/ExpenseForm.js
+++ expense-tracker-react/src/Components/NewExpenses/ExpenseForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ExpenseForm() {
   // Define common styling properties
@@ -9,38 +9,58 @@ export default function ExpenseForm() {
   const buttonClass =
     "font-bold text-white col-span-2 p-2 rounded-lg bg-gray-800 hover:bg-gray-700";
 
+  // const [itemName, setItemName] = useState("");
+  // const [itemPrice, setItemPrice] = useState("");
+  // const [itemDate, setItemDate] = useState("");
+
+  const onItemNameChange = (event) => {
+    console.log(event.target.value);
+  };
+  const onItemPriceChange = (event) => {
+    console.log(event.target.value);
+  };
+  const onItemDateChange = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
-      <div className={formContainerClass}>
+      <form className={formContainerClass}>
         <label className={labelClass}>Item Purchased</label>
         <input
+          onChange={onItemNameChange}
           className={inputClass}
           name="item-purchased"
           type="text"
           placeholder="Item Name"
+          // value={itemName}
           required
         />
         <label className={labelClass}>Item Price</label>
         <input
+          onChange={onItemPriceChange}
           className={inputClass}
           name="item-price"
           type="number"
           min={0}
           step={5}
           placeholder="₹ 100"
+          // value={itemPrice}
           required
         />
         <label className={labelClass}>Purchase Date</label>
         <input
+          onChange={onItemDateChange}
           className={inputClass}
           name="purchase-date"
           type="date"
+          // value={itemDate}
           required
         />
         <button className={buttonClass} type="submit">
           Add Expense
         </button>
-      </div>
+      </form>
     </div>
   );
 }
