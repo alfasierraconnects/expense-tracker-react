e53a956cbb397c1ecc606da5f4c0d47527814700
alfasierraconnects <alfasierraconnects@gmail.com>
2024-03-17 13:13:45 +0530

    Added filter functionality with conditional rendering

--- expense-tracker-react/src/Components/Expenses/Expenses.js
+++ expense-tracker-react/src/Components/Expenses/Expenses.js
@@ -3,25 +3,35 @@ import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 
 export default function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState("2021");
+  const [filteredExpenses, setFilteredExpenses] = useState(props.expenses);
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+    const filteredExpenses = props.expenses.filter((expense) => {
+      return expense.date.getFullYear().toString() === selectedYear;
+    });
+    setFilteredExpenses(filteredExpenses);
   };
+
   return (
     <div className="bg-slate-800 p-4 rounded-lg shadow-lg">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {props.expenses.map((expense, index) => (
-        <ExpenseItem
-          key={expense.id}
-          date={expense.date}
-          title={expense.title}
-          price={expense.price}
-        />
-      ))}
+      {filteredExpenses.length === 0 ? (
+        <p className="text-gray-200">No Expenses found</p>
+      ) : (
+        filteredExpenses.map((expense, index) => (
+          <ExpenseItem
+            key={expense.id}
+            date={expense.date}
+            title={expense.title}
+            price={expense.price}
+          />
+        ))
+      )}
     </div>
   );
 }
